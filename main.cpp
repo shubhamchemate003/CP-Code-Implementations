5266472bb5b7f040e76b2434ccce2e2ad1f5a4c7
Shubham <shubhamchemate3@gmail.com>
2022-02-16 00:09:03 +0530

    some changes in math

--- main.cpp
+++ main.cpp
@@ -12,26 +12,56 @@ using namespace std;
 #define sz(x) (int)(x).size()
 const long double PI = acos(-1);
 const int mod = 1e9 + 7, inf = 1e18;
-const int D = 2e5 + 10;
+const int D = 1e5 + 10;
 
-void solve_test() {
+vector<int> adj[D];
+int val[D];
+int ans;
 
+int dfs(int u, int p) {
+    int lcm = 1;
+    for (int v : adj[u])
+        if (v != p and sz(adj[v]) != 1) {
+            int l1 = dfs(v, u);
+            lcm = (l1 * lcm) / __gcd(l1, lcm);
+        }
+
+    int cnt = 0, sum = 0, mn = inf;
+    for (int v : adj[u])
+        if (v != p)
+            mn = min(mn, val[v]), cnt++, sum += val[v];
+
+    assert(lcm != 0);
+
+    int fval = mn / lcm * lcm;
+    val[u] = fval * cnt;
+    ans += sum - val[u];
+
+    // cout << u << ' ' << lcm << ' ' << cnt << ' ' << fval << '\n';
+
+    return lcm * cnt;
 }
 
+// notes
 signed main() {
     fastio
-#ifndef ONLINE_JUDGE
+
     freopen("../input1.txt", "r", stdin);
     freopen("../output1.txt", "w", stdout);
-#endif
 
-    int t = 1;
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> val[i];
 
-    cin >> t;
-
-    for (int i = 1; i <= t; i++) {
-        solve_test();
+    for (int i = 0; i < n - 1; i++) {
+        int x, y; cin >> x >> y;
+        adj[x].pb(y);
+        adj[y].pb(x);
     }
+
+    dfs(1, -1);
+
+    cout << ans << '\n';
 
     return 0;
 }
